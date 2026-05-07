d9b4579111f2923a907a831e7ca6c7706b1a3b87
mnmjimnz <56621226+mnmjimnz@users.noreply.github.com>
2026-05-06 18:27:50 -0600

    correccion en appsetting e inclusion de mapeo de entidades faltantes

--- ControlAsistenciaApi.Usecase/Helper/MappingProfile.cs
+++ ControlAsistenciaApi.Usecase/Helper/MappingProfile.cs
@@ -10,6 +10,16 @@ namespace ControlAsistenciaApi.Usecase.Helper
         {
             CreateMap<Alumno, AlumnoDto>();
             CreateMap<AlumnoDto, Alumno>();
+            CreateMap<Aula, AulaDto>();
+            CreateMap<AulaDto, Aula>();
+            CreateMap<Materia, MateriaDto>();
+            CreateMap<MateriaDto, Materia>();
+            CreateMap<Horario_d, Horario_dDto>();
+            CreateMap<Horario_dDto, Horario_d>();
+            CreateMap<Horario_h, Horario_hDto>();
+            CreateMap<Horario_hDto, Horario_h>();
+            CreateMap<Registro_asistencia, Registro_asistenciaDto>();
+            CreateMap<Registro_asistenciaDto, Registro_asistencia>();
         }
     }
 }
