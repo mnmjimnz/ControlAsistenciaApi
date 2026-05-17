b458a755988696325b64eaf4b9145c17ff716217
mnmjimnz <56621226+mnmjimnz@users.noreply.github.com>
2026-05-16 19:49:33 -0600

    ajuste en origin

--- ControlAsistenciaApi/Program.cs
+++ ControlAsistenciaApi/Program.cs
@@ -19,8 +19,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Libre", policy =>
     {
         policy
-            .WithOrigins("https://controlasistencia-front.onrender.com")
-            ///.WithOrigins("http://127.0.0.1:5500")
+            .WithOrigins(
+            "https://controlasistencia-front.onrender.com",
+            "http://127.0.0.1:5500")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
